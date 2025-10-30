e77268c14088d727d73be5a096094c01d56c393f
Asus-viper <ishowsagar777@gmail.com>
2025-10-30 17:15:30 +0530

    Added vanDetail page handling functionality

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+// params is just paramters short form 
 export default function VanDetail() {
+  const params =useParams()
+
+  React.useEffect( () => {
+    fetch(`/api/vans/${params.id}`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  } ,[params.id])
   return <h1>Van details comp is rendered!</h1>;
 }
