0059b0f6aac2aa2d400ffc90af12857c9e6acacb
Asus-viper <ishowsagar777@gmail.com>
2025-10-30 15:18:49 +0530

    commented out comments and made ahard copy in ref codespace

--- src/ref codespace/Vans copy.jsx
+++ src/ref codespace/Vans copy.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+// we want api to send us data ad fetch data by request just one time page renders,
+// so using useEffect hook and keeping the DeArr empty [],
+//  to make sure it does not depend on aything and,
+//  just run this block of code one time whenver site mounts/renders
 export default function Vans() {
   const [vans, setVans] = React.useState([]);
   React.useEffect(() => {
@@ -8,7 +12,8 @@ export default function Vans() {
       .then((data) => setVans(data.vans));
   }, []);
 
-
+  //   Mapped over data we get from api and returning all data from a variable,
+  //  to actually return it in return section of function
   const vanElements = vans.map((van) => (
     <div key={van.id} className="van-tile">
       <img src={van.imageUrl} />
@@ -23,7 +28,8 @@ export default function Vans() {
     </div>
   ));
 
-
+  // displayinf the logic here and unloading whats coming from it and,
+  //  rendering other things with it to render page we want.
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
