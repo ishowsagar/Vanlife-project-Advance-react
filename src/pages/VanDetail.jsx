323c6c8507bf633ed1934f510df0f828431e5cee
Asus-viper <ishowsagar777@gmail.com>
2025-10-30 17:16:11 +0530

    done

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,14 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-// params is just paramters short form 
+// params is just paramters short form
 export default function VanDetail() {
-  const params =useParams()
+  const [van, setVans] = useState(null);
+  const params = useParams();
 
-  React.useEffect( () => {
+  // it will load that van object from api server which would have this id
+  useEffect(() => {
     fetch(`/api/vans/${params.id}`)
-      .then(res => res.json())
-      .then(data => console.log(data))
-  } ,[params.id])
-  return <h1>Van details comp is rendered!</h1>;
+      .then((res) => res.json())
+      // we have to get inside van coz api gives us data with van and inside have our data
+      .then((data) => setVans(data.vans));
+  }, [params.id]);
+
+  return (
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} alt="" />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price">
+            <span>${van.price}</span>/day
+          </p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
