ec3d376f4945184f0d29992de380e4030b3fcee8
Asus-viper <ishowsagar777@gmail.com>
2025-10-31 10:41:43 +0530

    refactored folders and jsx files and added another bootstraped route for "Host".

--- src/index.jsx
+++ src/index.jsx
@@ -3,27 +3,30 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from "./pages/Vans";
+import Vans from "./pages/Vans/Vans";
 import "./server";
-import VanDetail from "./pages/VanDetail";
+import VanDetail from "./pages/Vans/VanDetail";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">#VANLIFE</Link>
-
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
         {/* -- at this path, it will render this component */}
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/vans" element={<Vans />} />
+
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Host/Dashboard.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+export default function Dashboard() {
+  return (
+    <>
+      <h1>This is Dashboard! component!</h1>
+      <Outlet />
+    </>
+  );
+}
