64e9cb053b400d24055daa60be7966c46bd7779c
Asus-viper <ishowsagar777@gmail.com>
2025-10-31 12:12:21 +0530

    Refactored code with hard css stylings and added Footer as shared ui comp after Outlet

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,10 +1,7 @@
-import { Outlet } from "react-router-dom";
-
 export default function Dashboard() {
   return (
     <>
       <h1>This is Dashboard! component!</h1>
-      <Outlet />
     </>
   );
 }
